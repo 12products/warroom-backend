bc9aae8a2b685f9d58d2fb22a4b3dba727de8319
Anthony Morris <amorriscode@gmail.com>
2022-02-21 12:05:42 -0800

    chore: remove whitespace

--- src/auth/current-user.decorator.ts
+++ src/auth/current-user.decorator.ts
@@ -4,7 +4,6 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 export const CurrentUser = createParamDecorator(
   (_data: unknown, context: ExecutionContext) => {
     const ctx = GqlExecutionContext.create(context);
-
     return ctx.getContext().req.user;
   },
 );
